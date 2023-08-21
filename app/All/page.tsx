44a67fe6fadf4966e39917986d85f29c9289bdd4
YASIR ZAHID <science5661@gmail.com>
2023-08-21 05:55:48 +0500

    working cart functionality and global cart and cart count contexts

--- app/All/page.tsx
+++ app/All/page.tsx
@@ -1,15 +1,44 @@
+'use client'
 import React from 'react'
 import Image from "next/image";
 import Products from '../DB/PRODUCTS';
+import { useCartCount } from "../CONTEXT/CONTEXT";
+import { useCart,CartItem} from '../CONTEXT/CARTCONTEXT';
 
 export default function AllSection() {
+  const { addToCart,cartItems } = useCart();
+  const { setNewCartCount } = useCartCount();
+  const handleAdd = (item: any) => {
+    const existingCartItem = cartItems.find((cartItem) => cartItem.productNumber === item[7]);
+    if (existingCartItem) {
+      alert('Item is already added to the cart');
+    } else {
+      const cartItem: CartItem = {
+        picture: item[0],
+        name: item[1],
+        category: item[2],
+        price: item[3],
+        discount: item[4],
+        gender: item[5],
+        priority: item[6],
+        productNumber: item[7],
+        quantity: '1',
+      };
+      addToCart(cartItem);
+      setNewCartCount(calculateItems(cartItems)+1);
+      alert('Item added to cart');
+    }
+  };
+  const calculateItems = (cart: any[]) => {
+    return cart.length;
+  };
   return (
     <div className="min-h-screen flex flex-col">
       <div className="h-full w-[88%] flex flex-wrap mx-auto mt-[5%]">
         {Products.map((product, index) => (
           <div key={index} className="w-[22%] h-[15%] mx-auto mt-[3%]">
             <div className="mx-[9%] h-full ">
-              <div className="h-[70%] w-full bg-teal-700 flex shadow-sm hover:shadow-xl hover:w-[98%] hover:h-[68%] transition-all duration-200 ease-in-out  mix-blend-multiply cursor-pointer rounded-sm hover:rounded-lg hover:ring-2 ring-lime-400 ">
+              <div className="h-[70%] w-full bg-teal-700 flex shadow-sm hover:shadow-xl hover:w-[98%] hover:h-[68%] transition-all duration-200 ease-in-out  mix-blend-multiply cursor-pointer rounded-sm hover:rounded-lg hover:ring-2 ring-lime-400 " onClick={()=>handleAdd(product)}>
                 <Image
                   src={product[0]}
                   width={600}
